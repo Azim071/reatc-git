6855c98491303505c6f80e1e4656d250ec5c3f14
Azim071 <azimdzalalbekov913@gmail.com>
2024-12-21 19:04:59 +0600

    header done

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <div>
+      <header>
+        <a href=""> HOUSE</a>
+      </header>
+    </div>
+  )  
 }
 
 export default App
